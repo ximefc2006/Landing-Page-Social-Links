7e156ccd31c46065a34bbf938984251d48b104f9
ximefc2006 <ximefc2006@gmail.com>
2026-07-11 16:24:27 -0600

    Regresar boton Maps debajo de la descripcion

--- script.js
+++ script.js
@@ -178,13 +178,11 @@ document.addEventListener('DOMContentLoaded', () => {
             if (props) {
                 const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${props.query}`;
                 this._div.innerHTML = `
-                    <div class="info-header">
-                        <h4>📍 ${props.name}</h4>
-                        <a href="${mapsUrl}" target="_blank" class="maps-btn">
-                            <i class="fa-solid fa-map-location-dot"></i> Maps
-                        </a>
-                    </div>
+                    <h4>📍 ${props.name}</h4>
                     <p class="store-description-desktop">${props.description}</p>
+                    <a href="${mapsUrl}" target="_blank" class="maps-btn">
+                        <i class="fa-solid fa-map-location-dot"></i> Abrir en Google Maps
+                    </a>
                 `;
                 this._div.style.display = 'block';
                 
